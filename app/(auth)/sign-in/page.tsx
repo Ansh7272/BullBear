1b6d045cf74a9ae150b190d9b07fb36548c628c6
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2025-11-07 10:19:42 +0530

    fix few typos and bugs on signinwithemail page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -26,8 +26,15 @@ const SignIn = () => {
     const onSubmit = async (data: SignInFormData) => {
         try {
             const result = await signInWithEmail(data);
-            if(result.success) router.push('/');
-        } catch (e) {
+            if (result.success) {
+                router.push('/');
+            } else {
+                toast.error('Sign in failed', {
+                    description: result.error || 'Failed to sign in.'
+                })
+
+            }
+        }catch (e){
             console.error(e);
             toast.error('Sign in failed', {
                 description: e instanceof Error ? e.message : 'Failed to sign in.'
